d8a7ba4b9515dad5c919a93410dd3fe67992e15c
“myrakuku” <kumannik.myra@gmail.com>
2025-10-20 12:31:51 +0800

    siteURL: change to real domin

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  siteUrl: 'http://localhost:3000', // 您的網站 URL
+  siteUrl: 'https://comeinmacbook.com/', // 您的網站 URL
   generateRobotsTxt: true, // 是否生成 robots.txt
 };
 
